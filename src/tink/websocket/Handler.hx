e9ecfb747969913d618bf7ceabb863ea9a0cb145
Kevin Leung <kevinresol@gmail.com>
2017-05-27 22:20:33 +0800

    Guard

--- src/tink/websocket/Handler.hx
+++ src/tink/websocket/Handler.hx
@@ -10,8 +10,10 @@ private typedef Impl = RealStream<Chunk>->IdealStream<Chunk>;
 
 @:callable
 abstract Handler(Impl) from Impl to Impl {
+	#if tink_tcp
 	public inline function asAcceptor():tink.tcp.Handler
 		return Acceptor.wrap(this);
 	public inline function asConnector(url:tink.Url):tink.tcp.Handler
 		return Connector.wrap(url, this);
+	#end
 }
