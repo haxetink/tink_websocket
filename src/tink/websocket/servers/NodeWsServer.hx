9e8f585167adcaafbd58f534e999a29a2609d9a5
Kevin Leung <kevinresol@gmail.com>
2018-09-20 12:09:25 +0800

    Add a server implementation based on the npm package "ws"

---
+++ src/tink/websocket/servers/NodeWsServer.hx
@@ -0,0 +1,88 @@
+package tink.websocket.servers;
+
+#if nodejs
+import tink.http.Request;
+import tink.websocket.Message;
+import tink.websocket.Server;
+import haxe.Constraints;
+import haxe.extern.EitherType;
+import js.html.ArrayBuffer;
+import js.node.http.IncomingMessage;
+
+using tink.CoreApi;
+
+class NodeWsServer implements Server {
+	public var connected(default, null):Signal<ConnectedClient>;
+	
+	var server:NativeServer;
+	
+	public function new(opt) {
+		server = new NativeServer(opt);
+		connected = Signal.generate(function(trigger) {
+			server.on('connection', function(socket, request:IncomingMessage) {
+				trigger((new NodeWsConnectedClient(
+					request.connection.remoteAddress,
+					IncomingRequestHeader.fromIncomingMessage(request),
+					socket
+				):ConnectedClient));
+			});
+		});
+	}
+	
+	public function close():Future<Noise> {
+		return Future.async(function(cb) {
+			server.close(cb.bind(Noise));
+		});
+	}
+}
+
+class NodeWsConnectedClient implements ConnectedClient {
+	
+	public var clientIp(default, null):String;
+	public var header(default, null):IncomingRequestHeader;
+	public var closed(default, null):Future<Noise>;
+	public var messageReceived(default, null):Signal<Message>;
+	
+	var socket:NativeSocket;
+	
+	public function new(clientIp, header, socket) {
+		this.clientIp = clientIp;
+		this.header = header;
+		this.socket = socket;
+		
+		closed = Future.async(function(cb) {
+			socket.once('close', cb.bind(Noise));
+		});
+		
+		messageReceived = Signal.generate(function(trigger) {
+			socket.on('message', function(message) {
+				trigger(Text(message));
+			});
+		});
+	}
+	
+	public function send(message:Message):Void {
+		socket.send(switch message {
+			case Text(v): v;
+			case Binary(v): v.toBytes().getData();
+		});
+	}
+	public function close():Void {
+		socket.close();
+	}
+}
+
+@:jsRequire('ws', 'Server')
+private extern class NativeServer {
+	function new(opt:{});
+	function on(event:String, f:Function):Void;
+	function close(f:Function):Void;
+}
+
+extern class NativeSocket {
+	function on(event:String, f:Function):Void;
+	function once(event:String, f:Function):Void;
+	function send(data:EitherType<String, ArrayBuffer>):Void;
+	function close():Void;
+}
+#end
