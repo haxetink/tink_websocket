8ecea923af4033f7d2f15d75170e446009499e33
Kevin Leung <kevinresol@gmail.com>
2019-12-19 23:34:12 +0800

    Update libraries

--- tests/RunTests.hx
+++ tests/RunTests.hx
@@ -6,7 +6,6 @@ import tink.unit.*;
 
 class RunTests {
 	static function main() {
-		tink.websocket.IncomingHandshakeRequestHeader;
 		Runner.run(TestBatch.make([
 			#if nodejs new TcpConnectorTest(), #end
 			#if nodejs new TcpAcceptorTest(), #end
